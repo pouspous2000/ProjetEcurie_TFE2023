cf279c661829f8ba4be7004f097edd47375f9a5f
pouspous2000 <63249471+pouspous2000@users.noreply.github.com>
2023-08-25 12:53:48 +0200

    testgç

--- tfe2023/Frontend/src/NavbarSidebar/NavbarSidebar.js
+++ tfe2023/Frontend/src/NavbarSidebar/NavbarSidebar.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Nav, Container, Row, Col, Navbar } from "react-bootstrap";
+import { Nav, Container, Row, Col, Navbar, Button, Offcanvas} from "react-bootstrap";
 import styles from './NavbarSidebar.module.css';
 import Stack from 'react-bootstrap/Stack';
 import { categories, subCategories } from './Component/Admin';
@@ -12,9 +12,13 @@ function NavbarSidebar() {
 
     const [showSidebar, setShowSidebar] = useState(false);
 
-    const handleToggleSidebar = () => {
-        setShowSidebar(!showSidebar);
-    };
+    const handleShow = () => {
+        setShowSidebar(true)
+    }
+
+    const handleClose = () => {
+        setShowSidebar(false)
+    }
 
     const handleCategoryClick = (categoryId) => {
         setActiveCategory(categoryId);
@@ -31,38 +35,51 @@ function NavbarSidebar() {
             (subCategory) => subCategory.categorie === activeCategory
         );
         return (
-            <Nav className={`col-3 col-sm-2 col-md-2 col-lg-2 ${styles.sidebarStyle}`}>
-                <Stack gap={3} style={{ fontSize: '20px' }} className="p-2">
-                    {categories.filter((categoryFilter) => categoryFilter.id === activeCategory).map((categoryMap) =>
-                        <div style={{ marginLeft: 'auto', marginRight: 'auto' }} key={categoryMap.id}>
-                            {categoryMap.name}
-                        </div>
-                    )}
-                    {subCategoryList.map((subCategory) => (
-                        <Nav.Item key={subCategory.id}>
-                            <div
-                                className={`${activeSubCategory === subCategory.id ? 'active' : ''} ${styles.button} p-2`}
-                                onClick={() => handleSubCategoryClick(subCategory.id)}
-                            >
-                                {subCategory.id}
-                            </div>
-                        </Nav.Item>
-                    ))}
-                </Stack>
-            </Nav>
+            <>
+                <Offcanvas show={showSidebar} onHide={handleClose} {...{backdrop: false}}>
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title></Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Stack gap={3} style={{ fontSize: '20px' }} className="p-2">
+                            {categories.filter((categoryFilter) => categoryFilter.id === activeCategory).map((categoryMap) =>
+                                <div style={{ marginLeft: 'auto', marginRight: 'auto' }} key={categoryMap.id}>
+                                    {categoryMap.name}
+                                </div>
+                            )}
+                            {subCategoryList.map((subCategory) => (
+                                <Nav.Item key={subCategory.id}>
+                                    <div
+                                        className={`${activeSubCategory === subCategory.id ? 'active' : ''} ${styles.button} p-2`}
+                                        onClick={() => {
+                                            handleSubCategoryClick(subCategory.id)
+                                            handleClose()
+                                        }}
+                                    >
+                                        {subCategory.id}
+                                    </div>
+                                </Nav.Item>
+                            ))}
+                        </Stack>
+                    </Offcanvas.Body>
+                </Offcanvas>
+            </>
         );
     };
 
     const renderContent = () => {
+       
         const subCategoryContent = subCategories.find(
             (subCategory) =>
                 subCategory.categorie === activeCategory && subCategory.id === activeSubCategory
         );
+ 
         return (
             <div className="container-fluid d-flex flex-column">
                 <div className="row">
                     {subCategoryContent && subCategoryContent.id === activeSubCategory && (
                         <div className="col-12 p-0">{subCategoryContent.content}</div>
+                       
                     )}
                 </div>
             </div>
@@ -73,7 +90,7 @@ function NavbarSidebar() {
         <>
             <Nav className={`justify-content-center grid text-center ${styles.navbarStyle}`}>
                 {categories.map((category) => (
-                    <Nav.Item key={category.id} className={styles.iconeStyle}>
+                    <Nav.Item key={category.id} className={`${styles.iconeStyle}`}>
                         <button
                             className={`${styles.iconeStyle} ${activeCategory === category.id ? 'active' : ''}`}
                             onClick={() => handleCategoryClick(category.id)}
@@ -88,7 +105,11 @@ function NavbarSidebar() {
                 <div className="row flex-grow-1">
                     {renderSubCategories()}
 
-                    <div className="col p-0">
+                    <div className="col p-0 position-relative ">
+                        <button onClick={handleShow} className={`d-block d-sm-none ${styles.buttonHamburger}`}>
+                            <i className="bi bi-list"></i>
+                        </button>
+
                         {renderContent()}
                     </div>
                 </div>
